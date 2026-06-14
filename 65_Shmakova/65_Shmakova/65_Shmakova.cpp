bf82378a44f5bfa868f168178a5c30b67ecafa97
Alina <gordeevana5@gmail.com>
2026-06-14 14:34:43 +0300

    Добавлена заглушка для функции findMaxSubmatrix

--- 65_Shmakova/65_Shmakova/65_Shmakova.cpp
+++ 65_Shmakova/65_Shmakova/65_Shmakova.cpp
@@ -1,6 +1,9 @@
 ﻿#include <iostream>
+#include "Functions.h"
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
+
+SubmatrixResult findMaxSubmatrix(const std::vector<std::vector<int>>& matrix, int rows, int cols) { return {}; }
