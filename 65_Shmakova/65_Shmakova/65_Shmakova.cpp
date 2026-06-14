32a36f2153afe0d0076f04dbddf09a8c20625896
Alina <gordeevana5@gmail.com>
2026-06-14 15:10:29 +0300

    Добавлена заглушка для функции scanDownwards

--- 65_Shmakova/65_Shmakova/65_Shmakova.cpp
+++ 65_Shmakova/65_Shmakova/65_Shmakova.cpp
@@ -9,3 +9,5 @@ int main()
 SubmatrixResult findMaxSubmatrix(const std::vector<std::vector<int>>& matrix, int rows, int cols) { return {}; }
 
 void calculateRightLengths(const std::vector<std::vector<int>>&, int, int, std::vector<std::vector<int>>&) {}
+
+void scanDownwards(const std::vector<std::vector<int>>&, int, int, int, const std::vector<std::vector<int>>&, SubmatrixResult&) {}
