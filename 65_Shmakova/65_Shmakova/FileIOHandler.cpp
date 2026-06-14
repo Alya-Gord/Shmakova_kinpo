ff55c931b8ddbe70ba280465e8ac20a8f3a82b2a
Alina <gordeevana5@gmail.com>
2026-06-15 02:17:26 +0300

    Добавлена реализация метода validateValue

--- 65_Shmakova/65_Shmakova/FileIOHandler.cpp
+++ 65_Shmakova/65_Shmakova/FileIOHandler.cpp
@@ -13,8 +13,32 @@ bool FileIOHandler::checkExtension(const std::filesystem::path& filename, const 
     return extensions.find(ext) != extensions.end();                                                        // Если расширение найдено в наборе допустимых - вернуть true, иначе false
 }
 
-// Метод валидации отдельного токена из файла.
-bool FileIOHandler::validateValue(const std::string& token, int& value, std::set<std::string>& error) { return false; }
+// Метод валидации отдельного токена из файла
+bool FileIOHandler::validateValue(const std::string& token, int& value, std::set<std::string>& error) {
+    if (token.find('.') != std::string::npos || token.find(',') != std::string::npos) {                    // Проверяем, является ли число дробным
+        error.insert(token);                                                                               // Добавляем ошибочный токен в множество ошибок
+        return false;                                                                                      // Возвращаем false
+    }
+    try {
+        size_t len_num;
+        value = std::stoi(token, &len_num);                                                               // Пытаемся конвертировать строку в число
+
+        if (len_num != token.length()) {                                                                  // Если конвертировалась не вся строка
+            error.insert(token);                                                                          // Добавляем токен с лишними символами в множество ошибок
+            return false;
+        }                                                                                                 // Возвращаем false
+
+        if (value < -1000 || value > 1000) {                                                              // Если число выходит за рамки требуемого диапазона
+            error.insert(token);                                                                          // Добавляем число в множество ошибок
+            return false;
+        }                                                                                                 // Возвращаем false
+    }
+    catch (...) {                                                                                         // Перехватываем исключения преобразования
+        error.insert(token);                                                                              // Добавляем неконвертируемый токен в множество
+        return false;                                                                                     // Возвращаем false
+    }
+    return true;                                                                                          // Если все проверки пройдены, возвращаем true
+}
 
 // Метод чтения матрицы из файла с полной валидацией данных.
 void FileIOHandler::readMatrix(const std::string& filename, int& cols, int& rows, std::vector<std::vector<int>>& matrix) {}
