d597a7df8bc50d12c82321bae05ab3c290a269d2
Alina <gordeevana5@gmail.com>
2026-06-15 02:01:05 +0300

    Добавлена реализация метода checkExtesion

--- 65_Shmakova/65_Shmakova/FileIOHandler.cpp
+++ 65_Shmakova/65_Shmakova/FileIOHandler.cpp
@@ -4,8 +4,14 @@
 #include <algorithm>
 #include <cctype>
 
-// Метод проверки расширения файла на соответствие заданному множеству расширений.
-bool FileIOHandler::checkExtension(const std::filesystem::path& filename, const std::unordered_set<std::string>& extensions) const { return false; }
+// Метод проверки расширения файла на соответствие заданному множеству расширений
+bool FileIOHandler::checkExtension(const std::filesystem::path& filename, const std::unordered_set<std::string>& extensions) const {
+    std::string ext = filename.extension().string();                                                        // Извлечь расширение файла из переданного пути
+
+    std::transform(ext.begin(), ext.end(), ext.begin(), [](unsigned char c) { return std::tolower(c); });   // Привести полученное расширение к нижнему регистру
+
+    return extensions.find(ext) != extensions.end();                                                        // Если расширение найдено в наборе допустимых - вернуть true, иначе false
+}
 
 // Метод валидации отдельного токена из файла.
 bool FileIOHandler::validateValue(const std::string& token, int& value, std::set<std::string>& error) { return false; }
