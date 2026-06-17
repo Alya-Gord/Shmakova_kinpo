c9f13ef548e603f425e0de6088a98c8c548e1da0
Alina <gordeevana5@gmail.com>
2026-06-17 06:36:34 +0300

    Добавлены тесты к функции записи матрицы в файл writeResult

---
+++ 65_Shmakova/Shmakova_tests/testWriteResult.cpp
@@ -0,0 +1,81 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "FileIOHandler.h"
+#include <vector>
+#include <string>
+#include <fstream>
+#include <cstdio>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Shmakova_tests
+{
+    TEST_CLASS(testWriteResult)
+    {
+    public:
+        // Проверка, что при корректных данных результат записывается в файл без выбрасывания исключений.
+        TEST_METHOD(CorrectDataNotThrowException) {
+            FileIOHandler handler;
+            std::string filename = "output.txt";
+            std::vector<std::vector<int>> matrix = {
+                {1, 2},
+                {3, 4}
+            };
+            SubmatrixResult res = { 0, 0, 1, 1, 1 };
+
+            try {
+                handler.writeResult(filename, matrix, res);
+
+                std::ifstream fileCheck(filename);
+                Assert::IsTrue(fileCheck.good());
+                fileCheck.close();
+
+                std::remove(filename.c_str());
+            }
+            catch (const Error& e) {
+                std::remove(filename.c_str());
+                Assert::Fail();
+            }
+        }
+
+        // Проверка, что при попытке записать результат в файл с недопустимым расширением выбрасывается исключение.
+        TEST_METHOD(ThrowExtensionException) {
+            FileIOHandler handler;
+            std::string filename = "output.csv";
+            std::vector<std::vector<int>> matrix = {
+                {1, 2},
+                {3, 4}
+            };
+            SubmatrixResult res = { 0, 0, 1, 1, 1 };
+
+            try {
+                handler.writeResult(filename, matrix, res);
+                Assert::Fail();
+            }
+            catch (const Error& e) {
+                Assert::IsTrue(e.type == INVALID_EXTENSION);
+            }
+        }
+
+        // Проверка, что при попытке записать результат в недопустимый файл выбрасывается исключение.
+        TEST_METHOD(ThrowExceptionOutputFile) {
+            FileIOHandler handler;
+            std::string filename = "output?.txt";
+            std::vector<std::vector<int>> matrix = {
+                {1, 2},
+                {3, 4}
+            };
+            SubmatrixResult res = { 0, 0, 1, 1, 1 };
+
+            try {
+                handler.writeResult(filename, matrix, res);
+
+                std::remove(filename.c_str());
+                Assert::Fail();
+            }
+            catch (const Error& e) {
+                Assert::IsTrue(e.type == OUTPUT_FILE_ERROR);
+            }
+        }
+    };
+}
