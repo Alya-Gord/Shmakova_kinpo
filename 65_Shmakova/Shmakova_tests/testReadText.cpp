e03a7f8863e7734b17e122f36c308f3529c9da41
Alina <gordeevana5@gmail.com>
2026-06-17 04:20:14 +0300

    Добавлены тесты для функции readText

---
+++ 65_Shmakova/Shmakova_tests/testReadText.cpp
@@ -0,0 +1,87 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "FileIOHandler.h"
+#include <fstream>
+#include <cstdio>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Shmakova_tests
+{
+    TEST_CLASS(testReadText)
+    {
+    public:
+		// Проверяем, что функция корректно читает строки из файла и возвращает их в виде вектора строк.
+        TEST_METHOD(CheckReadAndWrite) {
+            FileIOHandler handler;
+            std::string filename = "input.txt";
+
+            std::ofstream out(filename);
+            out << "2 2\n";
+            out << "1 2\n";
+            out << "3 4\n";
+            out.close();
+
+            std::vector<std::string> lines = handler.readText(filename);
+
+            Assert::AreEqual((size_t)3, lines.size());
+
+            Assert::AreEqual(std::string("2 2"), lines[0]);
+            Assert::AreEqual(std::string("1 2"), lines[1]);
+            Assert::AreEqual(std::string("3 4"), lines[2]);
+
+            std::remove(filename.c_str());
+        }
+
+		// Проверяем, что функция корректно фильтрует пустые строки и строки, состоящие только из пробелов.
+        TEST_METHOD(CheckDataFiltering) {
+            FileIOHandler handler;
+            std::string filename = "input.txt";
+
+            std::ofstream out(filename);
+            out << "2 2\n\n  \n1 2\n3 4\n";
+            out.close();
+
+            std::vector<std::string> lines = handler.readText(filename);
+
+            Assert::AreEqual((size_t)3, lines.size());
+
+            Assert::AreEqual(std::string("2 2"), lines[0]);
+            Assert::AreEqual(std::string("1 2"), lines[1]);
+            Assert::AreEqual(std::string("3 4"), lines[2]);
+
+            std::remove(filename.c_str());
+        }
+
+		// Проверяем, что функция выбрасывает исключение с правильным типом ошибки при попытке прочитать файл с недопустимым расширением.
+        TEST_METHOD(InvalidExtension) {
+            FileIOHandler handler;
+            try { handler.readText("input.bin"); Assert::Fail(); }
+            catch (const Error& e) { Assert::IsTrue(e.type == INVALID_EXTENSION); }
+        }
+
+		// Проверяем, что функция выбрасывает исключение с правильным типом ошибки при попытке прочитать несуществующий файл.
+        TEST_METHOD(FileNotFound) {
+            FileIOHandler handler;
+            try { handler.readText("input.txt"); Assert::Fail(); }
+            catch (const Error& e) { Assert::IsTrue(e.type == FILE_NOT_FOUND); }
+        }
+
+		// Проверяем, что функция выбрасывает исключение с правильным типом ошибки при попытке прочитать файл, который существует, но не содержит полезных данных.
+        TEST_METHOD(EmptyFile) {
+            FileIOHandler handler;
+            std::string filename = "input.txt";
+            std::ofstream out(filename); out.close();
+
+            try {
+                handler.readText(filename);
+                std::remove(filename.c_str());
+                Assert::Fail();
+            }
+            catch (const Error& e) {
+                std::remove(filename.c_str());
+                Assert::IsTrue(e.type == NO_INPUT_DATA);
+            }
+        }
+    };
+}
